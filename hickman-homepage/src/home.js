1d3b64d45ae19db5d0bd256185ba948c2d498012
Gretchyn Hickman <82916149+gretchyn-hickman@users.noreply.github.com>
2023-07-29 19:38:12 -0600

    moving things

--- hickman-homepage/src/home.js
+++ hickman-homepage/src/home.js
@@ -5,7 +5,7 @@ export default function Home() {
     return (
         <div className='App'>
             <h1>Gretchyn Hickman's Developer Homepage</h1>
-            <img src='../img/picture.png' alt='Gretchyn Hickman'></img>
+            <img src="/img/picture.png" alt='Gretchyn Hickman'></img>
         </div>
 
 
