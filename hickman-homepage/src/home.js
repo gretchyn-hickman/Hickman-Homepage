205c181880921bc8e3538e5dd381dc01d998b55a
Gretchyn Hickman <82916149+gretchyn-hickman@users.noreply.github.com>
2023-07-29 20:24:05 -0600

    changing things

--- hickman-homepage/src/home.js
+++ hickman-homepage/src/home.js
@@ -6,8 +6,12 @@ import picture from './img/picture.png'
 export default function Home() {
     return (
         <div className='App'>
-            <h1>Gretchyn Hickman's Developer Homepage</h1>
+            <h1 className='title'>Gretchyn Hickman's Developer Homepage</h1>
             <img src={picture} alt='Gretchyn Hickman' className='Profile'/>
+            <div className='para'>
+                <p>Hi! My name is Gretchyn Hickman. I am a 20 year old full-stack developer. I might be young but I am determined and creative when it comes to my projects. </p>
+            </div>
+            
         </div>
 
     )
