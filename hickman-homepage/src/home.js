3135d1b6ce0f4ef004aeefaba5d115ccbb462048
Gretchyn Hickman <82916149+gretchyn-hickman@users.noreply.github.com>
2023-07-29 19:22:59 -0600

    cleaning

--- hickman-homepage/src/home.js
+++ hickman-homepage/src/home.js
@@ -3,8 +3,8 @@ import './App.css';
 
 export default function Home() {
     return (
-        <div>
-            <h1>Hello, this is home</h1>
+        <div className='App'>
+            <h1>Gretchyn Hickman's Developer Homepage</h1>
         </div>
 
 
