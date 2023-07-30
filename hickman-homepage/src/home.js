1ef9910928168112e8798b291e7b8cba8403ab45
Gretchyn Hickman <82916149+gretchyn-hickman@users.noreply.github.com>
2023-07-29 19:44:59 -0600

    uhh

--- hickman-homepage/src/home.js
+++ hickman-homepage/src/home.js
@@ -1,16 +1,14 @@
 import React from 'react';
 import './App.css';
+import './img/picture'
+
 
 export default function Home() {
     return (
         <div className='App'>
             <h1>Gretchyn Hickman's Developer Homepage</h1>
-            <img src="/img/picture.png" alt='Gretchyn Hickman'></img>
+            <img src="/img/picture.png" alt='Gretchyn Hickman'/>
         </div>
-
-
-
-
 
     )
 }
