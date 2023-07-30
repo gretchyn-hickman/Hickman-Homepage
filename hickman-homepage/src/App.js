f90cf5c18ccfda9c253edb950cf1dffc4b1cf671
Gretchyn Hickman <82916149+gretchyn-hickman@users.noreply.github.com>
2023-07-29 19:13:37 -0600

    adding home

--- hickman-homepage/src/App.js
+++ hickman-homepage/src/App.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import './App.css';
 import TheNav from './nav.js';
+import Home from './home'
 
 export default function App() {
   return (
-
-  
+    <div>
       <TheNav />
-
+      <Home/>
+    </div>
       
   );
 }
---
+++ hickman-homepage/src/home.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+
+export default function Home() {
+    return (
+        <div>
+            <h1>Hello, this is home</h1>
+        </div>
+
+
+
+
+
+    )
+}
