fc1f78a9239ec6eef4304f1e0fc7f9fbcf02b258
Gretchyn Hickman <82916149+gretchyn-hickman@users.noreply.github.com>
2023-07-29 19:04:47 -0600

    playing with css

--- hickman-homepage/src/nav.js
+++ hickman-homepage/src/nav.js
@@ -9,24 +9,20 @@ import NavDropdown from 'react-bootstrap/NavDropdown';
 export default function TheNav() {
   return (
     <div>
-    <Navbar expand="lg" className="bg-body-tertiary">
+    <Navbar expand="lg" className="color-one">
       <Container>
-        <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+        <Navbar.Brand href="#home">Gretchyn Hickman</Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
             <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#link">Link</Nav.Link>
-            <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-              <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+            <Nav.Link href="#about">About Me</Nav.Link>
+            <NavDropdown title="My Projects" id="basic-nav-dropdown">
+              <NavDropdown.Item href="#action/3.1">Weather App</NavDropdown.Item>
               <NavDropdown.Item href="#action/3.2">
-                Another action
+                Dictionary App
               </NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-              <NavDropdown.Divider />
-              <NavDropdown.Item href="#action/3.4">
-                Separated link
-              </NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.3">Old Homepage</NavDropdown.Item>
             </NavDropdown>
           </Nav>
         </Navbar.Collapse>
